20bc2fa0a1a8e2288f5cf910e5b560cc8a0c86ae
RuxCavescu <rux.cavescu@gmail.com>
2022-09-01 10:24:04 +0200

    counter done

--- Counter/index.js
+++ Counter/index.js
@@ -1,6 +1,15 @@
 let count = 0;
+const countEl = document.getElementById('count-el');
+const saveEl = document.getElementById('save-el');
 
 function increment() {
-  count = count + 1;
-  document.getElementById('count-el').innerText = count;
+  count += 1;
+  countEl.textContent = count;
+}
+
+function save() {
+  let countStr = +count + ' - ';
+  saveEl.textContent += countStr;
+  count = 0;
+  countEl.textContent = count;
 }
